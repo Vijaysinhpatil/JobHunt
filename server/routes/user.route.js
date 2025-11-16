e4789a6c435d623537aecd18d3c520734311a3aa
vijaysinh Patil <149512603+Vijaysinhpatil@users.noreply.github.com>
2025-11-16 07:07:04 +0530

    route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,12 +2,18 @@ import express, { Router } from 'express'
 import {Login, Logout, register, updateProfile} from '../controller/user.controller.js'
 import  isAuthenticated  from '../middleware/user.auth.js'
 import { singleUpload } from '../middleware/multer.js'
+import { ensureDbConnection } from '../middleware/dbConnection.js';
 const router = express.Router()
+
+
+// Apply DB connection middleware to all routes
+router.use(ensureDbConnection);
 
 router.route('/register').post( singleUpload, register) 
 router.route('/login').post(Login)
 router.route('/logout').get(Logout)
 router.route('/updateProfile').post(isAuthenticated ,singleUpload , updateProfile)
+
 
 
 export default router;
